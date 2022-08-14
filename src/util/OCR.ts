f8a5fb01043876cce8d07b0709ecbd7a549249ff
RyanL123 <ryan.lichangrui@gmail.com>
2022-08-14 15:47:20 -0400

    hide variables in env file

--- src/util/OCR.ts
+++ src/util/OCR.ts
@@ -1,7 +1,3 @@
-const ENDPOINT_URL = "https://adventureworksocr.cognitiveservices.azure.com/";
-const OCR_ENDPOINT = `${ENDPOINT_URL}vision/v3.2/ocr?language=en&detectOrientation=true`;
-const SUBSCRIPTION_KEY = "f2c848871684499cb002561d34ebff64";
-
 function makeBlob(dataURL: string) {
     var BASE64_MARKER = ";base64,";
     if (dataURL.indexOf(BASE64_MARKER) == -1) {
@@ -26,13 +22,14 @@ function makeBlob(dataURL: string) {
 
 export async function getTextFromImage(url: string) {
     const isBase64 = url.substring(0, 4) === "data";
-    return await fetch(OCR_ENDPOINT, {
+    return await fetch(process.env.REACT_APP_OCR_ENDPOINT, {
         method: "POST",
         headers: {
             "Content-Type": isBase64
                 ? "application/octet-stream"
                 : "application/json",
-            "Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY,
+            "Ocp-Apim-Subscription-Key":
+                process.env.REACT_APP_OCR_SUBSCRIPTION_KEY,
         },
         body: isBase64 ? makeBlob(url) : `{"url":"${url}"}`,
     }).then((data) => {
@@ -57,24 +54,23 @@ export async function getTextFromImage(url: string) {
     });
 }
 
-const CV_ENDPOINT_URL =
-    "https://adventureworksobjectrecog-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/8dc317ee-4eb2-4ca2-8515-7841e836b3fa/classify/iterations/Iteration1/url";
-
-const CV_ENDPOINT_IMAGE =
-    "https://adventureworksobjectrecog-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/8dc317ee-4eb2-4ca2-8515-7841e836b3fa/classify/iterations/Iteration1/image ";
-
 export async function getPartType(url: string) {
     const isBase64 = url.substring(0, 4) === "data";
-    return await fetch(isBase64 ? CV_ENDPOINT_IMAGE : CV_ENDPOINT_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": isBase64
-                ? "application/octet-stream"
-                : "application/json",
-            "Prediction-Key": "49abfcf6092c420284a00c0805cc64f7",
-        },
-        body: isBase64 ? makeBlob(url) : `{"url":"${url}"}`,
-    }).then((data) => {
+    return await fetch(
+        isBase64
+            ? process.env.REACT_APP_CV_IMAGE_ENDPOINT
+            : process.env.REACT_APP_CV_LINK_ENDPOINT,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": isBase64
+                    ? "application/octet-stream"
+                    : "application/json",
+                "Prediction-Key": process.env.REACT_APP_CV_PREDICTION_KEY,
+            },
+            body: isBase64 ? makeBlob(url) : `{"url":"${url}"}`,
+        }
+    ).then((data) => {
         return data.json().then((data) => {
             let finalPredictions = data.predictions.filter((prediction) => {
                 return prediction.probability >= 0.8; // 80% confidence or more
